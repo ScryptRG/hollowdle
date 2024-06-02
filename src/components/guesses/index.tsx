b1662b2eff0fe498584df236b6fe21be126cb770
Renan <renanalmeida201612@gmail.com>
2024-06-02 03:00:57 -0300

    trying to fix animation

--- src/components/guesses/index.tsx
+++ src/components/guesses/index.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import styles from "./guesses.module.css";
 import "animate.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface CharactersDataInterface {
   name: string;
@@ -21,6 +21,51 @@ interface GuessesProps {
 
 export default function Guesses(props: GuessesProps) {
   const { hints, index, guesses } = props;
+
+  const guessItems = [
+    {
+      title: "Character",
+      icon: `characters/${hints.characterIcon}`,
+      desc: hints.name,
+    },
+    {
+      title: "Location",
+      icon: `locations/${hints.locationIcon}`,
+      desc: hints.location,
+    },
+    {
+      title: "Health",
+      icon: "health.png",
+      desc: hints.name,
+    },
+    {
+      title: "Geo Drop",
+      icon: "geo.png",
+      desc: hints.name,
+    },
+    {
+      title: "Gender",
+      icon: "gender.png",
+      desc: hints.name,
+    },
+  ];
+
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    // Remove the class
+    setAnimate(false);
+
+    // Force a reflow to restart the animation
+    const element = document.getElementById("animatable");
+    if (element) {
+      void element.offsetWidth;
+    }
+
+    // Re-add the class
+    setAnimate(true);
+  });
+
   return (
     <div
       className={`relative flex flex-wrap gap-8 justify-center mt-24 text-white`}
@@ -30,90 +75,25 @@ export default function Guesses(props: GuessesProps) {
         alt=""
         className="absolute -top-8 -left-6 z-10"
       />
-      <div className="flex flex-col gap-2 items-center">
-        <h1 className="font-['Trajan'] text-xl">Character</h1>
-        <div
-          className={`${index === 0 && styles["guessesItem-1"]} group ${
-            styles.guessCard
-          }`}
-        >
-          <Image
-            src={require(`../../assets/images/icons/characters/${hints.characterIcon}`)}
-            alt=""
-            height={70}
-            className="group-hover:-translate-y-1 duration-200"
-          />
-          <h1 className={styles.guessDesc}>{hints.name}</h1>
+      {guessItems.map((e, i) => (
+        <div className="flex flex-col gap-2 items-center" key={i}>
+          <h1 className="font-['Trajan'] text-xl">{e.title}</h1>
+          <div
+            id="animatable"
+            className={`${
+              index === 0 && (animate ? styles["guessesItem"] : "")
+            } group ${styles.guessCard}`}
+          >
+            <Image
+              src={require(`../../assets/images/icons/${e.icon}`)}
+              alt=""
+              height={70}
+              className="group-hover:-translate-y-1 duration-200"
+            />
+            <h1 className={styles.guessDesc}>{e.desc}</h1>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col gap-2 items-center">
-        <h1 className="font-['Trajan'] text-xl">Location</h1>
-        <div
-          className={`${index === 0 && styles["guessesItem-2"]} 
-            border border-red-800 bg-red-700 bg-opacity-15 group hover:bg-opacity-20
-            ${styles.guessCard}
-            `}
-        >
-          <Image
-            src={require(`../../assets/images/icons/locations/${hints.locationIcon}`)}
-            alt=""
-            height={50}
-            className="group-hover:-translate-y-1 duration-200"
-          />
-          <h1 className={styles.guessDesc}>{hints.location}</h1>
-        </div>
-      </div>
-      <div className="flex flex-col gap-2 items-center">
-        <h1 className="font-['Trajan'] text-xl">Health</h1>
-        <div
-          className={`${index === 0 && styles["guessesItem-3"]} 
-            border border-red-800 bg-red-700 bg-opacity-15 group hover:bg-opacity-20
-            ${styles.guessCard}
-            `}
-        >
-          <Image
-            src={require(`../../assets/images/icons/health.png`)}
-            alt=""
-            height={70}
-            className="group-hover:-translate-y-1 duration-200"
-          />
-          <h1 className={styles.guessDesc}>{hints.health}</h1>
-        </div>
-      </div>
-      <div className="flex flex-col gap-2 items-center">
-        <h1 className="font-['Trajan'] text-xl">Geo Drop</h1>
-        <div
-          className={`${index === 0 && styles["guessesItem-4"]} 
-            border border-red-800 bg-red-700 bg-opacity-15 group hover:bg-opacity-20
-            ${styles.guessCard}
-            `}
-        >
-          <Image
-            src={require(`../../assets/images/icons/geo.png`)}
-            alt=""
-            height={70}
-            className="group-hover:-translate-y-1 duration-200"
-          />
-          <h1 className={styles.guessDesc}>{hints.geo_drop}</h1>
-        </div>
-      </div>
-      <div className="flex flex-col gap-2 items-center">
-        <h1 className="font-['Trajan'] text-xl">Gender</h1>
-        <div
-          className={`${index === 0 && styles["guessesItem-5"]} 
-            border border-red-800 bg-red-700 bg-opacity-15 group hover:bg-opacity-20
-            ${styles.guessCard}
-            `}
-        >
-          <Image
-            src={require(`../../assets/images/icons/gender.png`)}
-            alt=""
-            height={70}
-            className="group-hover:-translate-y-1 duration-200"
-          />
-          <h1 className={styles.guessDesc}>{hints.gender}</h1>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
